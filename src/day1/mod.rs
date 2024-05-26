a9727491b7db8bd81a3e76a3ff429e2e7c804b3e
Somebody32x2 <samuelshuster@gmail.com>
2024-05-26 17:49:21 -0400

    Proper Structure

---
+++ src/day1/mod.rs
@@ -0,0 +1,18 @@
+use std::fs;
+pub(crate) fn solve() {
+    let input: String = fs::read_to_string("src\\day1\\input.txt").expect("Failure to Read File");
+    let mut sum: i32 = 0;
+    for line in input.split("\n") {
+        let mut mostRecentNum: Option<u8> = Option::None;
+        for char in line.chars() {
+            if char.is_numeric() {
+                if mostRecentNum.is_none() {
+                    sum += (char as u8 - '0' as u8) as i32 * 10;
+                }
+                mostRecentNum = Option::Some(char as u8 - '0' as u8);
+            }
+        }
+        sum += (mostRecentNum.unwrap_or(0) as i32);
+    }
+    println!("{}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use std::fs;
+mod day1;
 
 fn main() {
-    let input: String = fs::read_to_string("input.txt").expect("Failure to Read File");
-    let mut sum: i32 = 0;
-    for line in input.split("\n") {
-        let mut mostRecentNum: Option<u8> = Option::None;
-        for char in line.chars() {
-            if char.is_numeric() {
-                if mostRecentNum.is_none() {
-                    sum += (char as u8 - '0' as u8) as i32 * 10;
-                }
-                mostRecentNum = Option::Some(char as u8 - '0' as u8);
-            }
-        }
-        sum += (mostRecentNum.unwrap_or(0) as i32);
-    }
-    println!("{}", sum);
+    day1::solve();
 }
