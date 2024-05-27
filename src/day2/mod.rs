39804e3bcf41a1420392a835d05c803f0a31673f
Somebody32x2 <samuelshuster@gmail.com>
2024-05-27 17:00:02 -0400

    Day 2.5

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -4,26 +4,27 @@ use std::fs;
 pub(crate) fn solve() {
     let input: String = fs::read_to_string("src\\day2\\input.txt").expect("Failure to Read File");
 
-    let max_cubes: HashMap<&str, u32> = [("red", 12), ("green", 13), ("blue", 14)].into_iter().collect();
-
-    let mut game_id_sum: u32 = 0;
-    for mut line in input.split("\n") {
-        let game_strs: Vec<&str> = line.split(":").collect();
-        let game_id: u32 = game_strs[0].split(" ").collect::<Vec<&str>>()[1].parse().unwrap();
-        let mut valid_game = true;
-        for pull_str in game_strs[1].split(";").collect::<Vec<&str>>() {
-            for color_num_str in pull_str.split(",").collect::<Vec<&str>>() {
-                let (num_str, color) = color_num_str.trim().split_once(" ").unwrap();
-                println!("#{game_id}: num_str {}, color {}", num_str, color);
+    let mut power_sum: u32 = 0;
+    for mut line in input.split('\n') {
+        let game_strs: Vec<&str> = line.split(':').collect();
+        let game_id: u32 = game_strs[0].split(' ').collect::<Vec<&str>>()[1]
+            .parse()
+            .unwrap();
+        let mut num_cubes: HashMap<&str, u32> = [("red", 0), ("green", 0), ("blue", 0)]
+            .into_iter()
+            .collect();
+        for pull_str in game_strs[1].split(';').collect::<Vec<&str>>() {
+            for color_num_str in pull_str.split(',').collect::<Vec<&str>>() {
+                let (num_str, color) = color_num_str.trim().split_once(' ').unwrap();
+                // println!("#{game_id}: num_str {}, color {}", num_str, color);
                 let num: u32 = num_str.parse().unwrap();
-                if num > *max_cubes.get(color).unwrap() {
-                    valid_game = false;
+                if num > *num_cubes.get(color).unwrap() {
+                    num_cubes.entry(color).and_modify(|x| *x = num);
                 }
             }
         }
-        if valid_game {
-            game_id_sum += game_id;
-        }
+
+        power_sum += num_cubes.values().product::<u32>();
     }
-    println!("{}", game_id_sum);
+    println!("{}", power_sum);
 }
