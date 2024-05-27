3412d7bcf0e9cb2a930ede3a323bf2d6bb3e94bb
Somebody32x2 <samuelshuster@gmail.com>
2024-05-26 20:46:53 -0400

    Day 1.5

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,18 +1,48 @@
 use std::fs;
+const NUM_STRS: [&str; 10] = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+];
+const NUM_REP_STRS: [&str; 10] = [ // This is a bad solution but works without significant compute required for substring matching
+    "z0ero",
+    "o1ne",
+    "t2wo",
+    "t3hree",
+    "fo4ur",
+    "fi5ve",
+    "s6ix",
+    "se7ven",
+    "eig8ht",
+    "ni9ne"
+];
 pub(crate) fn solve() {
-    let input: String = fs::read_to_string("src\\day1\\input.txt").expect("Failure to Read File");
+    let mut input: String = fs::read_to_string("src\\day1\\input.txt").expect("Failure to Read File");
+    // Part 1.5
+    for (i, num) in NUM_STRS.iter().enumerate() {
+        input = input.replace(*num, NUM_REP_STRS[i]);
+    }
+    println!("{}", input);
+
     let mut sum: i32 = 0;
     for line in input.split("\n") {
-        let mut mostRecentNum: Option<u8> = Option::None;
+        let mut most_recent_num: Option<u8> = None;
         for char in line.chars() {
             if char.is_numeric() {
-                if mostRecentNum.is_none() {
+                if most_recent_num.is_none() {
                     sum += (char as u8 - '0' as u8) as i32 * 10;
                 }
-                mostRecentNum = Option::Some(char as u8 - '0' as u8);
+                most_recent_num = Some(char as u8 - '0' as u8);
             }
         }
-        sum += (mostRecentNum.unwrap_or(0) as i32);
+        sum += (most_recent_num.unwrap_or(0) as i32);
     }
     println!("{}", sum);
 }
